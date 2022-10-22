00bf90497ac28d61c63062992708dfa8fe2e0879
Naotsun <1999naotsun0411@gmail.com>
2022-10-22 13:41:26 +0900

    Create repository

---
+++ Package.bat
@@ -0,0 +1,5 @@
+md .\Release
+
+.\DistributionTool.exe -b -i .\Sources\ -o .\Release\ 
+
+pause
---
+++ Sources/GraphPrinter/models/CounterSettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GraphPrinter.Models
+{
+    public class CounterSettingsModel
+    {
+        public int Counter { get; set; } = 0;
+    }
+}
