5422644381ce7bfcc1cf6c05c3572ec143ff2d4e
Naotsun <1999naotsun0411@gmail.com>
2022-11-01 13:06:10 +0900

    Add test web socket code

--- Sources/GraphPrinter/GraphPrinterAction.cs
+++ Sources/GraphPrinter/GraphPrinterAction.cs
@@ -2,28 +2,66 @@
 
 using StreamDeckLib;
 using StreamDeckLib.Messages;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Fleck;
 
 namespace GraphPrinter
 {
     [ActionUuid(Uuid="com.naotsun.graphprinter.clipboard.all")]
     public class GraphPrinterAction : BaseStreamDeckActionWithSettingsModel<Models.WebSocketSettingsModel>
     {
+        private List<IWebSocketConnection> AllSockets = new();
+        private WebSocketServer Server;
+
         public override async Task OnKeyUp(StreamDeckEventPayload args)
         {
-            await Manager.SetTitleAsync(args.context, args.context);
-            await Manager.SetSettingsAsync(args.context, SettingsModel);
+            await Manager.SetTitleAsync(args.context, "OnKeyUp");
         }
 
         public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
         {
-            await Manager.SetTitleAsync(args.context, "Test");
+            await Manager.SetTitleAsync(args.context, "OnDidReceiveSettings");
+            
+            RestartServer();
         }
 
         public override async Task OnWillAppear(StreamDeckEventPayload args)
         {
             await base.OnWillAppear(args);
-            await Manager.SetTitleAsync(args.context, "Test");
+            await Manager.SetTitleAsync(args.context, "OnWillAppear");
+            
+            RestartServer();
+        }
+
+        private void RestartServer()
+        {
+            Server = new WebSocketServer(SettingsModel.ServerURL);
+            Server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    AllSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    AllSockets.Remove(socket);
+                    if (AllSockets.Count == 0)
+                    {
+                        Server.Dispose();
+                    }
+                };
+                socket.OnMessage = message =>
+                {
+                    foreach (var otherSocket in AllSockets)
+                    {
+                        if (otherSocket != socket)
+                        {
+                            otherSocket.Send(message);
+                        }
+                    }
+                };
+            });
         }
     }
 }
--- Sources/GraphPrinter/models/WebSocketSettingsModel.cs
+++ Sources/GraphPrinter/models/WebSocketSettingsModel.cs
@@ -5,7 +5,7 @@ namespace GraphPrinter.Models
     // The IDE recommends adding abstract, but if we do, it will not work properly.
     public class WebSocketSettingsModel
     {
-        public string ServerURL { get; set; }
-        public string ServerProtocol { get; set; }
+        public string ServerURL { get; set; } = "ws://127.0.0.1:3000";
+        public string ServerProtocol { get; set; } = "ws";
     }
 }
